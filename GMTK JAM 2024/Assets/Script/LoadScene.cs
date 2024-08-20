318c50e43a916ea84939ac50656d761da0a9acde
mickychin <61143890+mickychin@users.noreply.github.com>
2024-08-20 18:24:29 +0700

    setting

--- GMTK JAM 2024/Assets/Script/LoadScene.cs
+++ GMTK JAM 2024/Assets/Script/LoadScene.cs
@@ -1,17 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
 
 public class LoadScene : MonoBehaviour
 {
+    public AudioMixer audioMixer;
     public void LoadGame()
     {
         SceneManager.LoadScene("ActualGame");
     }
 
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void LoadSetting()
+    {
+        SceneManager.LoadScene("Setting");
+    }
+
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    public void Togglemusic(bool TurnOn)
+    {
+        GameObject.FindGameObjectWithTag("Music").GetComponent<AudioSource>().enabled = TurnOn;
+    }
+
+    public void SetVolume(float volume)
+    {
+        audioMixer.SetFloat("Volume", volume);
+    }
+
+    public void SetMusicVolume(float volume)
+    {
+        GameObject.FindGameObjectWithTag("Music").GetComponent<AudioSource>().volume = volume;
     }
 }
